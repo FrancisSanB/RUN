82bb11132cd662ef875ec51ecc1da2e72f26029b
Joshua You <awesomejosh1337@gmail.com>
2017-07-18 22:50:42 -0700

    better movement system for desktop

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -2,25 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovePlayer : MonoBehaviour {
-	public float speed = 0.1f;
-	public Rigidbody2D playerRigidbody;
+enum MovementType
+{
+    Mobile,
+    Desktop
+}
 
-	public bool moveLeft;
-	public bool moveRight;
+public class MovePlayer : MonoBehaviour {
+	public float speed = 50f;
+    public float jumpSpeed = 3f;
+
+    public Rigidbody2D playerRigidbody;
+
+    bool moveLeft;
+	bool moveRight;
+
+    MovementType type;
 
 	void Start() {
 		playerRigidbody = GetComponent<Rigidbody2D> ();
-	}
+	    
+        type = Application.isMobilePlatform ? MovementType.Mobile : MovementType.Desktop;
+    }
 
 	void Update() {
-		if (moveLeft && !moveRight) {
-			playerRigidbody.AddForce (Vector2.left * speed);
-		}
 
-		if (moveRight && !moveLeft) {
-			playerRigidbody.AddForce (Vector2.right * speed);
-		}
+        switch (type) {
+            
+            case MovementType.Mobile:
+
+                // TODO make mobile movement less shitty
+                if (moveLeft && !moveRight)
+                {
+                    playerRigidbody.AddForce(Vector2.left * speed);
+                }
+
+                if (moveRight && !moveLeft)
+                {
+                    playerRigidbody.AddForce(Vector2.right * speed);
+                }
+                break;
+            
+            case MovementType.Desktop:
+                playerRigidbody.AddForce(Vector2.right * speed * Input.GetAxis("Horizontal") * Time.deltaTime);
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    playerRigidbody.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+                }
+                break;
+        }
+
+
 
 	}
 
