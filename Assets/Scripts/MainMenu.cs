3317c6c82c9f324a726cd00637b183bdf93ae0b3
Joshua You <awesomejosh1337@gmail.com>
2017-07-22 11:20:27 -0700

    further cleanup

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+	public Canvas mainCanvas;
+	public Canvas exitCanvas;
+	public Button desert;
+	public Button exit;
+
+	// Use this for initialization
+	void Start () {
+		mainCanvas = mainCanvas.GetComponent<Canvas> ();
+		exitCanvas = exitCanvas.GetComponent<Canvas> ();
+		desert = desert.GetComponent<Button> ();
+		exit = exit.GetComponent<Button> ();
+
+	}
+	
+	public void exitPress() {
+		//TODO exit pressing system, bring exit canvas
+
+	}
+
+	public void desertGame() {
+		SceneManager.LoadScene (1);
+
+	}
+
+	public void storePress() {
+		//TODO make a store, in another update
+
+	}
+
+	public void creditsPress() {
+		//TODO make a credits in the before release MAKE SURE IT INCLUDES THE FONT
+		//dafont.com/lemon-milk.font 
+
+	}
+}
