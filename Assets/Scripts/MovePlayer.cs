cf29c82917f578bc3162205c672cadce589eac7f
Joshua You <youjoshua12@gmail.com>
2017-07-29 14:02:48 -0700

    movement

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -2,72 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-enum MovementType{Mobile, Desktop}
+public class MovePlayer : MonoBehaviour
+{
+    [HideInInspector] public bool facingRight = true;
+    [HideInInspector] public bool jump = false;
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+    public float jumpForce = 1000f;
+    public Transform groundCheck;
 
-public class MovePlayer : MonoBehaviour {
-	public float speed = 50f;
-    public float jumpSpeed = 3f;
+    private bool grounded = false;
+    private Animator anim;
 
-    public Rigidbody2D playerRigidbody;
+    Rigidbody2D playerRigidbody;
 
     bool moveLeft;
-	bool moveRight;
-
-    MovementType type;
-
-	void Start() {
-		playerRigidbody = GetComponent<Rigidbody2D> ();
-	    
-        type = Application.isMobilePlatform ? MovementType.Mobile : MovementType.Desktop;
+    bool moveRight;
+    
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        anim = GetComponent <Animator>();
+        playerRigidbody = GetComponent<Rigidbody2D>();
     }
 
-	void Update() {
+    void Update()
+    {
 
-        /*switch (type) {
-            
-            case MovementType.Mobile:
-                // TODO make mobile movement less shitty
-                if (moveLeft && !moveRight)
-                {
-                    playerRigidbody.AddForce(Vector2.left * speed);
-                }
-
-                if (moveRight && !moveLeft)
-                {
-                    playerRigidbody.AddForce(Vector2.right * speed);
-                }
-                break;
-            
-            case MovementType.Desktop:*/
-       	playerRigidbody.AddForce(Vector2.right * speed * Input.GetAxis("Horizontal") * Time.deltaTime);
-        if (Input.GetKeyDown(KeyCode.Space))
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        anim.SetBool("Grounded", grounded);
+        if (Input.GetButtonDown("Jump") && grounded)
         {
-		playerRigidbody.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            jump = true;
         }
-		//break;
-        //}
+
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(h));
+
+        if (h * playerRigidbody.velocity.x < maxSpeed)
+            playerRigidbody.AddForce(Vector2.right * h * moveForce);
+
+        if (Mathf.Abs(playerRigidbody.velocity.x) > maxSpeed)
+            playerRigidbody.velocity = new Vector2(Mathf.Sign(playerRigidbody.velocity.x) * maxSpeed, playerRigidbody.velocity.y);
+
+        if (h > 0 && !facingRight)
+            Flip();
+        else if (h < 0 && facingRight)
+            Flip();
+
+        if (jump)
+        {
+            anim.SetTrigger("Jump");
+            playerRigidbody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            jump = false;
+        }
+    }
 
 
-
-	}
-
-	public void MoveLeft() {
-		moveLeft = true;
-		Debug.Log ("MOVING LEFT");
-	}
-
-	public void StopLeft() {
-		moveLeft = false;
-		Debug.Log ("STOP LEFT");
-	}
-
-	public void MoveRight() {
-		moveRight = true;
-		Debug.Log ("MOVE RIGHT");
-	}
-
-	public void StopRight() { 
-		moveRight = false;
-		Debug.Log ("STOP RIGHT");
-	}
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
